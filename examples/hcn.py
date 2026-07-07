0ad6fac7f3301b986770401618b002adf7fe58df
ibonfil <ibonfil@gmail.com>
2026-07-07 15:44:52 -0600

    Inclusión de DL-FIND para optimización estructural

--- examples/hcn.py
+++ examples/hcn.py
@@ -2,18 +2,19 @@ import pynof
 
 mol = pynof.molecule("""
 0 1
-  O  
-  H 1 0.96 
-  H 1 0.96 2 97 
+C
+N  1 1.164
+H  1 4.636 2 179.998
 """)
 
-p = pynof.param(mol,"cc-pvdz")
+p = pynof.param(mol,"def2-tzvpd")
 
 p.ipnof = 8
 
 p.RI = True
-p.gpu = True
+#p.gpu = True
+p.title = 'hcn'
 
 pynof.dlfind_opt_geo(mol,p,ncons=1,spec=[1,1,1,
-                                         2,2,1,3,0,
+                                         1,1,3,0,0,
                                          1,1,1])
